88fca3a9be1630974599b63538023220e0920fa4
gabrielfahl <122305815+gabrielfahl@users.noreply.github.com>
2023-07-04 23:21:19 -0300

    exercicio 2

--- ex2.py
+++ ex2.py
@@ -1,1 +1,8 @@
-#  ex2
+frase = input('Digite a frase para ser retornada sem caracteres repetidos: ')
+frase_nova = ''
+
+for c in frase:
+    if c not in frase_nova:
+        frase_nova += c
+
+print(frase_nova)
